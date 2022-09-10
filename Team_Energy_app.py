4c8b617f2aa16bfb25ff6d1035b022fafc19e5c0
Jordan Harris <jordan.lee.harris@icloud.com>
2022-09-10 14:10:55 +0100

    Update Team_Energy_app.py

--- Team_Energy_app.py
+++ Team_Energy_app.py
@@ -9,7 +9,7 @@ from PIL import Image # Image manipulation
 import plotly as py
 
 # Page configuraion
-st.set_page_config(page_title="Test Streamlit App", page_icon=":smiley:", layout="wide", initial_sidebar_state="expanded")
+st.set_page_config(page_title="Team Energy Le Wagon Project", page_icon=":smiley:", layout="wide", initial_sidebar_state="expanded")
 
 # fucntion to load lottie animation
 def load_lottieurl(url):
@@ -25,14 +25,23 @@ lottie_coding_Data_Science_Animation = load_lottieurl("https://assets5.lottiefil
 
 # ---| ALL FUNCTIONS |---
 # Header section
-
 with st.container():
     st.title("Streamlit App")
     st.subheader("This is a Test web app for LeWagon Team Energy")
     st.write("This is a test app for LeWagon Team Energy to test Streamlit")
     st.write("[Le Wagon Home Page](https://www.lewagon.com)")
-    st
+
+# ---| USER INPUTS|---
+
+# ---| CALLING API |---
 
 with st.container():
     st.write("---")
     st_lottie(lottie_coding_Data_Science_Animation, height=600)
+
+# ---| DATA VISUALISATION |---
+
+# ---| FOOTER |---
+
+
+# ---| END OF CODE |---
