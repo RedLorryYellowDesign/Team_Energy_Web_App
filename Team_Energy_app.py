5b58f9b7bbe61303b13d65f33531d1860cfbf953
Jordan Harris <jordan.lee.harris@icloud.com>
2022-09-16 01:00:56 +0100

    Update + Tests

--- Team_Energy_app.py
+++ Team_Energy_app.py
@@ -1,141 +1,318 @@
-
-# ---| ALL IMPORT LIBRARIES |---
-# Base Streamlit Libraries
+# ---| TEAM ENERGY STREAMLIT WEB APP |--->>>>
+# >>>> Site Structure & Desing
+# .1234567890.123456789012345.123456789012345.123456789012345.123456789012345.
+# |SIDE BAR  |               |               |               |               |
+# +----------+---------------+---------------+---------------+---------------+ HEADER SECTION
+# |Page Title|Tabs/1/2/3/4/5 |               |               |               |
+# |Sub Title +---------------+---------------+---------------+---------------+ BODY SECTION
+# |About     |               |                                               |
+# |About     |               |                                               |
+# |          |               |                                               |
+# |          |               |                                               |
+# |          |               |                                               |
+# |          |               |                                               |
+# +----------+---------------+---------------+---------------+---------------+  FOOTER SECTION
+# |About     |               |               |               |               |
+# |          |               |               |               |               |
+# +----------+---------------+---------------+---------------+---------------+
+#  SIDE BAR
+# ---| ALL IMPORT LIBRARIES |--->>>>
+# ---| BASE STREAMLIT LIBRARIES |--->>>>
+from decimal import FloatOperation
+from pickle import FALSE
 import requests # Allows use of URL imports
 import streamlit as st # Allows compatibility with Streamlit
 from streamlit_lottie import st_lottie # Allows lottie animation
 from PIL import Image # Image manipulation
-# Data Science Libraries
+# ---| DATASCIANCE LIBRARIES |--->>>>
 import plotly as py
+import plotly.express as px
+import plotly.graph_objects as go
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-<<<<<<< Updated upstream
-=======
-from Team_Energy.predict  import *
-from Team_Energy.data import create_data, get_weather
-
->>>>>>> Stashed changes
-# Page configuraion
-st.set_page_config(page_title="Team Energy Le Wagon Project", page_icon=":smiley:", layout="wide", initial_sidebar_state="expanded")
-
-# Use local css file to style the app
+import joblib
+import plotly.figure_factory as ff
+# ---| API ON/OFF DEPENDENT LIBRARIES |--->>>>
+API_MODE = False
+if API_MODE == False:
+    from Team_Energy.predict  import *
+    from Team_Energy.data import create_data, get_weather
+# ---| VERIABLES |--->>>>
+Show_Graph = False
+Show_Lode = False
+forcaset = 0
+# ---| PAGE CONFIGURATION |--->>>>
+st.set_page_config(page_title="Team Energy Le Wagon Project", page_icon=":zap:", layout="wide", initial_sidebar_state="expanded")
+# ---| LOAD CSS FOR STYLEING |---
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-
 local_css("style/style.css")
-
-# fucntion to load lottie animation
+# ---| LOTTIE ANIMATION FUNCTIONS |--->>>>
 def load_lottieurl(url):
     r = requests.get(url)
     if r.status_code != 200:
         return None
     return r.json()
-
-# ---| IMPORTING ASSEST |---
-# Importing lottie animation
+# ---| CALL API FROM GCP |--->>>>
+def API_REQUESTS(url):
+    if API_MODE == True:
+        r = requests.get(url)
+        if r.status_code != 200:
+            return None
+        return r.json()
+    else:
+        st.write("API Call is set to False, Please Enable API Call")
+# ---| POST API |--->>>>
+def API_POST(url, data):
+    if API_MODE == True:
+        r = requests.post(url, data=data)
+        if r.status_code != 200:
+            return None
+        return r.json()
+    else:
+        st.write("API Call is set to False, Please Enable API Call")
+# ---| CALLING PREDICT MODEL |--->>>>
+def Predict_Model(User_Tarrif_Selected,User_Group_Selected ):
+    if User_Tarrif_Selected != "" and User_Group_Selected != " ":
+            st.write("Model will be called here")
+            name = User_Group_Selected
+            tariff = User_Tarrif
+            # ---| IMPORT JOBLIT MODEL |--->>>>
+            filename = f'Team_Energy/model_{name}_{tariff}.joblib'
+            m = joblib.load(filename)
+            st.write("model loaded succcessfully")
+            # ---| PREDICTING |--->>>>
+            train_df, test_df = create_data(name = name, tariff = tariff)
+            train_wd, test_wd = get_weather(train_df, test_df)
+            forecast = forecast_model(m,train_wd,test_wd,add_weather=True)
+    else :
+        st.write("Please Select both a Tarrif and Group")
+# ---| SEABORN PLOT FUNCTIONS |--->>>>
+def seabon_line_plot(x, y, title, xlabel, ylabel, hue=None):
+    fig_01, ax = plt.subplots(figsize=(10, 6))
+    sns.lineplot(x=x, y=y, hue=hue)
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    st.pyplot(fig_01)
+# ------>>>>
+def seabon_bar_plot(df, x, y, title, xlabel, ylabel, hue=None):
+    fig_02, ax = plt.subplots(figsize=(10, 6))
+    sns.barplot(data=df, x=x, y=y, hue=hue)
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    st.pyplot(fig_02)
+# ---| PLOTLY PLOT FUNCTIONS |--->>>>
+def plotly_line_plot(df, x, y, title, xlabel, ylabel, hue=None):
+    fig_03 = py.line(df, x=x, y=y, title=title, labels={x:xlabel, y:ylabel}, color=hue)
+    st.plotly_chart(fig_03)
+# ---| IMPORTING LOTTIE ASSEST |---
 lottie_coding_Data_Science_Animation = load_lottieurl("https://assets5.lottiefiles.com/packages/lf20_xl3sktpc.json")
+Team_Lottie_Animation = load_lottieurl("https://assets5.lottiefiles.com/packages/lf20_vctzcozn.json")
+Loding_Animation = load_lottieurl("https://assets5.lottiefiles.com/packages/lf20_ibxFWH.json")
+# ---| IMPORTING IMAGES |--->>>>
 
-# ---| MODEL WILL BE CALLE HERE |---
-# def model_call(User_Tarrif_Selected,User_Group_Selected): # model will be called here
-#     return Model_Result
+# ---| SIDE BAR |--->>>>
+with st.sidebar:
+    st.title("Streamlit App")
 
-# ---| SEANORN DEFULT PLOT |---
-x1 = np.linspace(0, 20, 100)
-fig3 = sns.lineplot(data=x1)
+# lineplot = st.sidebar.selectbox("Select Plot Type", ["Line Plot", "Bar Plot", "Line Plot with Plotly"])
 
-# Header section
+
+# ---| HEADER SECTION |--->>>>
 with st.container():
-    Col_1,Col_2 = st.columns((3,1))
-    with Col_1:
+    Header_col_1, Header_col_2, Header_col_3, Header_col_4 = st.columns(4)
+    with Header_col_1:
         st.title("Streamlit App")
         st.subheader("This is a Test web app for LeWagon Team Energy")
         st.write("This is a test app for LeWagon Team Energy to test Streamlit")
         st.write("[Le Wagon Home Page](https://www.lewagon.com)")
-
-    with Col_2:
-        st_lottie(lottie_coding_Data_Science_Animation, speed=1, height=200, key="initial")
-
-# ---| USER INPUTS|---
-
+    with Header_col_2:
+        st.empty()
+    with Header_col_3:
+        st.empty()
+    with Header_col_4:
+        st.empty()
+# ---| MAIN SECTION |--->>>>
 with st.container():
-    Col_1,Col_2 = st.columns((1,2))
-    with Col_1:
-        # tarrif
-        st.subheader("Pease Select your Tarrif Type")
-        User_Tarrif_Selected = st.selectbox('Pick one', ["","Fixed Tarrif", "Variable Tarrif"])
+    Main_col_1, Main_col_2 = st.columns((2,4))
+    with Main_col_1:
+        # Insert containers separated into tabs:
+        tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["First Question", "Second Question", "Third Question", "Fourth Question", "Fifth Question", "Sixth Question"])
+        with tab1:
+            st.write("Please Select your Tarrif Type")
+            User_Tarrif_Selected = st.selectbox('Pick one', ["","Fixed Tarrif", "Variable Tarrif"])
+            if User_Tarrif_Selected == "Fixed Tarrif":
+                User_Tarrif = "Std"
+                st.write("You have selected Fixed Tarrif")
+            elif User_Tarrif_Selected == "Variable Tarrif":
+                User_Tarrif = "ToU"
+                st.write("You have selected Variable Tarrif")
+            else:
+                st.write("Please select a tarrif")
 
-        if User_Tarrif_Selected == "Fixed Tarrif":
-            User_Tarrif = "Std"
-        elif User_Tarrif_Selected == "Variable Tarrif":
-            User_Tarrif = "ToU"
-        else:
-            st.write("Please select a tarrif")
+        with tab2:
+            st.write("Please Select your Group Type")
+            User_Group_Selected = st.selectbox('Pick one', [" ","A","E","Q"])
 
-        # Group
-        st.subheader("Pease Select your Group")
-        User_Group_Selected = st.selectbox('Pick one', [" ","A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q"])
-        if User_Group_Selected != " ":
-            st.write("Please select a Group")
+        with tab3:
+            st.write("Question 1 Text")
+            Question_1 = st.selectbox('Pick one', [" ","A","E","Q"], key="Question_1")
+            if Question_1 != " ":
+                st.write("Please select an option")
+
+        with tab4:
+            st.write("Question 1 Text")
+            Question_2 = st.selectbox('Pick one', [" ","A","E","Q"], key="Question_2")
+            if Question_2 != " ":
+                st.write("Please select an option")
+        with tab5:
+            st.write("Question 1 Text")
+            Question_3 = st.selectbox('Pick one', [" ","A","E","Q"], key="Question_3")
+            if Question_3 != " ":
+                st.write("Please select an option")
+        with tab6:
+            st.write("Question 1 Text")
+            Question_4 = st.selectbox('Pick one', [" ","A","E","Q"], key="Question_4")
+
+            if Question_4 != " ":
+                st.write("Please select an option")
+        with tab7:
+            st.write("Question 1 Text")
+            Question_5 = st.selectbox('Pick one', [" ","A","E","Q"], key="Question_5")
+            if Question_5 != " ":
+                st.write("Please select an option")
 
         # Submit Button
-        if st.button("Submit") and User_Tarrif_Selected != "" and User_Group_Selected != " ":
-            st.write("Model will be called here")
-            name = User_Group_Selected
-            tariff = User_Tarrif
-            # Joblib import model
-            filename = f'model_{name}_{tariff}.joblib'
-            m = joblib.load(filename)
-            print('model loaded succcessfully')
-            # Predictint here
-            train_df, test_df = create_data(name = name, tariff = tariff)
-            train_wd, test_wd = get_weather(train_df, test_df)
+        if st.button("Submit"):
+        # Predict_Model(User_Tarrif_Selected,User_Group_Selected )
+            if User_Tarrif_Selected != "" and User_Group_Selected != " ":
+                st.write("Model will be called here")
+                name = User_Group_Selected
+                tariff = User_Tarrif
+                # ---| IMPORT JOBLIT MODEL |--->>>>
+                filename = f'Team_Energy/model_{name}_{tariff}.joblib'
+                m = joblib.load(filename)
+                st.write("model loaded succcessfully")
+                # ---| PREDICTING |--->>>>
+                train_df, test_df = create_data(name = name, tariff = tariff)
+                train_wd, test_wd = get_weather(train_df, test_df)
+                forecast = forecast_model(m,train_wd,test_wd,add_weather=True)
+                Show_Lode = True
+                Show_Graph = True
+    # with Main_col_2:
+        # ---| PLOTTING |--->>>>
+        # while Show_Lode == True:
+        # st_lottie(Loding_Animation, speed=1, height=200, key="Loding_Animation")
+    with Main_col_2:
+        if Show_Graph == True:
+            with st.spinner('Wait for it...'):
+                fig_1 = plt.figure(figsize=(15, 6))
+                sns.lineplot(x=forecast['ds'],y=forecast['yhat'],label='Forecast');
+                sns.lineplot(x=test_df['DateTime'],y=test_df['KWH/hh'],label='Actual');
+                fig_2 = figure(figsize=(15,6))
+                sns.lineplot(x=test_wd['DateTime'],y=test_wd['temperature'],label='Weather');
+                st.pyplot(fig_1)
+                st.pyplot(fig_2)
 
-            forecast_model(m,train_wd,test_wd,add_weather=False)
-        else:
-            st.write("Opps something went wrong")
-<<<<<<< Updated upstream
+                x_axis = np.linspace(0, 1, 100)
+                y_axis = np.random.randn(100) - 5
+
+                st_fig = go.Figure()
+                obj = go.Scatter(
+                    x = x_axis,
+                    y = y_axis,
+                    line = dict(color='firebrick', width=4)
+                )
+                st_fig.add_trace(obj)
+
+                st_fig.update_layout(
+                    title='Line Plot',
+                    xaxis_title='X axis',
+                    yaxis_title='Y axis'
+                    )
+                Show_Lode = False
+                # Plotly_Graph = go.Figure()
+                # fig_43 = px.line(
+                #     x=forecast['ds'],
+                #     y=forecast['yhat'],
+                #     markers=True,
+                #     title ='Forecast')
+
+                # Plotly_Graph.add_trace(fig_43)
+
+                # st.plotly_chart(Plotly_Graph)
+
+                # #Lines mode
+                # go_fig = go.Figure()
+                # obj_lines = go.Scatter(
+                #     x = x_axis,
+                #     y = y0,
+                #     mode = 'lines',
+                #     name = 'lines'
+                # )
+
+                # #Lines + markers mode
+                # obj_lm = go.Scatter(
+                #     x = x_axis,
+                #     y = y1,
+                #     mode = 'lines+markers',
+                #     name = 'lines+markers'
+                # )
+
+                # #Markers mode
+                # obj_markers = go.Scatter(
+                #     x = x_axis,
+                #     y = y2,
+                #     mode = 'markers',
+                #     name = 'markers'
+                # )
+
+                # #Adding traces
+                # go_fig.add_trace(obj_lines)
+                # go_fig.add_trace(obj_lm)
+                # go_fig.add_trace(obj_markers)
+
+                # #Create the plot
+                # go_fig.show()
+            st.success('Done!')
+            st.plotly_chart(st_fig)
 
 
-        with Col_2:
-            st.write("---")
-            st_lottie(lottie_coding_Data_Science_Animation, height=600)
-
-        with Col_3:
-            st.write("---")
-            st.write("---")
-            st.write("---")
-
-=======
->>>>>>> Stashed changes
-# ---| DATA VISUALISATION |---
-
-# plot sns line graph into streamlit
-with Col_2:
-<<<<<<< Updated upstream
-    st.pyplot()
-    st.set_option('deprecation.showPyplotGlobalUse', False)
-=======
-    st.subheader("Plotly Graph")
-    # make a plotly Graph of forcast
-    fig = px.line(forcast, x="date", y="consumption", title='Consumption Forecast')
-    st.plotly_chart(fig)
->>>>>>> Stashed changes
-
-# ---| FOOTER |---
-# footer section with contrabuters and links
+# ---| FOOTER SECTION|--->>>>
 with st.container():
     st.write("---")
-    col1, col2, col3= st.columns(3)
-    with col1:
-        st.write("The Team")
-        st.write(" ")
-    with col2:
-        st.write(" ")
-    with col3:
-        st.write(" ")
-
-# ---| END OF CODE |---
+    Flooter_col_1, Flooter_col_2, Flooter_col_3, Flooter_col_4 = st.columns(4)
+    with Flooter_col_1:
+        st.subheader("The Team")
+        st.write("This app was created by the Team Energy. The Team Energy is a group of 4 students from Le Wagon Data Science Bootcamp. The Team Energy is made up of the following members:")
+    with Flooter_col_2:
+        st_lottie(Team_Lottie_Animation, speed=1, key="i")
+    with Flooter_col_3:
+        st.write("Zenan Ahmed")
+        st.write("[ZenanAH](https://github.com/ZenanAH)")
+        st.write("---")
+        st.write("Chris Cockerill")
+        st.write("[Ruston3](https://github.com/Ruston3)")
+    with Flooter_col_4:
+        st.write("Jordan Harris")
+        st.write("[RedLorryYellowDesign](https://github.com/RedLorryYellowDesign)")
+        st.write("---")
+        st.write("Jordan Haynes")
+        st.write("[haynesj1](https://github.com/haynesj1)")
+# ---| END OF CODE |--->>>>
+        # my_slider_val = st.slider('Quinn Mallory', 1, 88)
+        # st.text('Fixed width text')
+        # st.markdown('_Markdown_') # see *
+        # st.latex(r''' e^{i\pi} + 1 = 0 ''')
+        # st.write('Most objects') # df, err, func, keras!
+        # st.write(['st', 'is <', 3]) # see *
+        # st.title('My title')
+        # st.header('My header')
+        # st.subheader('My sub')
+        # st.code('for i in range(8): foo()')
+        # st.markdown("This **word** is bold. This <em>word</em> is italic.")
